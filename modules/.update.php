67fe9e03e707e1388ad2c059417f275a1329a982
Dedi Rohadi <155202658+Felyxh@users.noreply.github.com>
2023-12-31 06:15:45 +0700

    Update .update.php

--- modules/.update.php
+++ modules/.update.php
@@ -9,12 +9,6 @@ function logo() {
 \033[01;33m
 
 
-\033[01;31m      _\033[01;33m ____    _
-     \033[01;31m(_)\033[01;33m  _ \  | |_ _ __ __ _  ___ ___ _ __
-     | | |_) | | __| '__/ _` |/ __/ _ \ '__|
-     | |  __/  | |_| | | (_| | (_|  __/ |
-     |_|_|      \__|_|  \__,_|\___\___|_|
-
 
    \033[01;37m}\033[01;31m----------------------------------------\033[01;37m{
 }\033[01;31m-------------- \033[01;32mTrack IPLocation\033[01;31m --------------\033[01;37m{
@@ -26,13 +20,13 @@ EOL;
 
 function upd() {
   logo();
-  echo "\n\033[01;32mUpdating IP-Tracer.........\033[01;37m\n\n";
+  echo "\n\033[01;32mUpdating Lacak-ip.........\033[01;37m\n\n";
   sleep(1);
-  system("cd ~/ && git clone https://github.com/rajkumardusad/IP-Tracer.git");
-  system("cd ~/ && sudo git clone https://github.com/rajkumardusad/IP-Tracer.git");
-  system("cd ~/IP-Tracer && sh install");
+  system("cd ~/ && git clone https://github.com/github.com/Felyxh/lacak-ip");
+  system("cd ~/ && sudo git clone https://github.com/Felyxh/lacak-ip");
+  system("cd ~/lacak-ip && sh install");
   logo();
-  echo "\n\033[01;32m              IP-Tracer updated !!!\033[01;37m\n";
+  echo "\n\033[01;32m              Lacak-ip updated !!!\033[01;37m\n";
   sleep(1);
 }
 upd();
